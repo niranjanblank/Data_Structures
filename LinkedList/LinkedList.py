1f6f73dbff45e2e2c657a1108eb6f119f45e8277
Niranjan Shah <niranjanshah474@gmail.com>
2023-11-08 20:25:45 +1100

    Linkedlist with append method created

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,48 @@
+from typing import Type
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return f"Data: {self.data} next: {self.next}"
+
+
+class LinkedList:
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return f"Items: {self.items}"
+
+    def __len__(self):
+        return len(self.items)
+
+    def append(self, node: Node):
+        if self.items:
+            self.items[-1].next = node
+            self.items.append(node)
+            return
+        self.items.append(node)
+
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    node1 = Node('A')
+    node2 = Node('B')
+    node3 = Node('C')
+    node4 = Node('D')
+
+    linked_list.append(node1)
+    linked_list.append(node2)
+    linked_list.append(node3)
+    linked_list.append(node4)
+    item = linked_list.items[0]
+
+    while item.next:
+        print(f"Current Data: {item.data}, Next Data: {item.next.data}")
+        item = item.next
+    else:
+        print(f"Current Data: {item.data}, Next Data: {item.next}")
